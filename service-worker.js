5a3438b1cd6b8147e9174a8495563c203ad82713
lo-de <lodeplay@gmail.com>
2025-06-09 22:02:51 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "petanque-cache-v1";
+const CACHE_NAME = "petanque-cache-v2";
 const urlsToCache = [
   "index.html",
   "manifest.json",
@@ -16,5 +16,19 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
   );
 });
